3a9e6fda75bf5a5fd29559a68a29589543b184ad
Kyle Son <sonkyle0420@gmail.com>
2026-02-18 02:31:35 -0800

    added express

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
